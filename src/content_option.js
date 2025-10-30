69d235c19684549dc1d4089fe322f8df0e60e163
TranDuc <tranthaitrongduc@gmail.com>
2025-10-30 11:18:59 +0700

    My website TranDuc

--- src/content_option.js
+++ src/content_option.js
@@ -1,162 +1,129 @@
-const logotext = "JOHN";
+const logotext = "DucTran";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Tran Duc",
+    description: "I’m DucTran data scientist _ Full stack devloper,currently working in VietNam",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m DucTran",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop mobile apps in VietNam",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Don’t strive to be better than someone else — strive to be better than who you were yesterday. True growth comes from within, not from comparison.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "I'm a third-year student majoring in Physics and Computer Science. I've been self-learning web development and want to go deeper into backend and frontend to gain hands-on experience toward becoming a fullstack developer.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
-];
+// const worktimeline = [{
+//     jobtitle: "Designer of week",
+//     where: "YAdfi",
+//     date: "2020",
+// },
+// {
+//     jobtitle: "Designer of week",
+//     where: "Jamalya",
+//     date: "2019",
+// },
+// {
+//     jobtitle: "Designer of week",
+//     where: "ALquds",
+//     date: "2019",
+// },
+// ];
 
 const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+    name: "Python",
+    value: 50,
+},
+{
+    name: "SQL",
+    value: 65,
+},
+{
+    name: "Javascript",
+    value: 70,
+},
+{
+    name: "React",
+    value: 60,
+},
+{
+    name: "Jquery",
+    value: 70,
+},
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+    title: "UI & UX Design",
+    description: "Creating modern and user-friendly interfaces with Figma, focusing on usability, seamless experience, and visual balance across devices.",
+},
+{
+    title: "Mobile Apps",
+    description: "Building responsive and intuitive Android apps with Kotlin and Android Studio — blending performance with great design.",
+},
+{
+    title: "WordPress Design",
+    description: "Building modern, responsive WordPress websites with a focus on performance, customization, and an eye-catching look.",
+},
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    img: "https://picsum.photos/400/?grayscale",
+    description: "My CV",
+    link: "https://staticfile-858aa.wasmer.app/",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "Weather dashboard",
+    link: "https://weather-alpha-green.vercel.app/",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Comming soon",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "Comming soon",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "Comming soon",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Comming soon",
+    link: "#",
+},
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_EMAIL: "tranthaitrongduc@gmail.com",
+    YOUR_FONE: "(+36) 368 483 397",
+    description: "If you’re interested in my work or would like to collaborate, don’t hesitate to get in touch. I’d be happy to connect and discuss ideas further via email. ",
+    YOUR_SERVICE_ID: "service_ge7bh66",
+    YOUR_TEMPLATE_ID: "template_irdqdyp",
+    YOUR_USER_ID: "LovQo64TwtfO5WoX6",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/DucTran231104",
+    facebook: "https://www.facebook.com/duke.tran.2024/",
+    linkedin: "https://www.linkedin.com/in/tr%E1%BA%A7n-%C4%91%E1%BB%A9c-37306136b/",
 };
 export {
     meta,
     dataabout,
     dataportfolio,
-    worktimeline,
+    // worktimeline,
     skills,
     services,
     introdata,
